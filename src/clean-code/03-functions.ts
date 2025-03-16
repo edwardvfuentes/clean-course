d71feaa3bac7ad959acec24b4516a5618760baac
Edward <vattico@gmail.com>
2025-03-16 21:32:33 +0100

    Terminado ejercicio 03

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,46 @@
+(() => {
+
+    // función para obtener información de una película por Id
+    function getMovieInfo( movieId: string ) {
+        console.log({ movieId });
+    }
+
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getMovieActors( movieId: string ) {
+        console.log({ movieId });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getActorBio( actorId: string ) {
+        console.log({ actorId });
+    }
+    
+    interface movieInfo {
+        title: string;
+        description: string;
+        rating: number;
+        cast: string[];
+    }
+
+    // Crear una película
+    function createMovie({title, description, rating, cast}: movieInfo) {
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createActor( fullName: string, birthDate: Date ): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( fullName === 'fernando' ) return false;
+
+        console.log('Crear actor');
+        return true;        
+
+    }
+
+    
+
+
+})();
